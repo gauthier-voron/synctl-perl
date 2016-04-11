018bc2fd47be0c0e02b9b6c197bfc1f663006a59
Gauthier Voron <gauthier.voron@lip6.fr>
2016-04-11 10:20:46 +0200

    chore(build): add a h2xs file hierarchy
    Add a standard Perl module file hierarchy where we can follow the
    usual build process.

--- lib/Synctl.pm
+++ lib/Synctl.pm
@@ -1,14 +1,23 @@
 package Synctl;
 
+use 5.022001;
 use strict;
 use warnings;
 
-use Carp;
-
 require Exporter;
 
-my @ISA = qw(Exporter);
-my @EXPORT_OK = qw(backend);
+our @ISA = qw(Exporter);
+
+our %EXPORT_TAGS = (
+    'all' => [ qw(backend) ]
+    );
+
+our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
+
+our @EXPORT = qw(
+);
+
+our $VERSION = '0.1.0';
 
 
 sub backend
@@ -47,3 +56,57 @@ sub backend
     
     return $class->new($target);
 }
+
+
+1;
+__END__
+
+=head1 NAME
+
+Synctl - Make local or remote incremental backups
+
+=head1 SYNOPSIS
+
+  use Synctl;
+
+  my $backend = Synctl::backend('/mnt/backup');
+  my $backend = Synctl::backend('file:///mnt/backup');
+  my $backend = Synctl::backend('ssh://backup@www.remote.net:/var/backup');
+
+  foreach my $backup ($backend->list()) {    # list every backups made at this
+      printf("%s\n", $backup);               #   server before
+  }
+
+  $backend->client('/');           # set the root of files to backup
+  $backend->exclude('/mnt');       # ignore some files during the backup
+  $backend->dryrun(1);             # do not make any write on disk
+  $backend->verbose(1);            # explain what is going on
+
+  $backend->send();                # make a new backup
+  $backend->recv();                # recover from the last backup
+  $backend->recv('2016-03');       # recover from the last backup of March 2016
+
+  use Synctl::Config;
+
+  # Parse a configuration file named 'config-name' which can be found either in
+  # '~/.config/synctl' or in '~/.synctl'.
+
+  my $config = Synctl::Config->new('config-name', '~/.config/synctl',
+      '~/.synctl');
+
+  my $backend = $config->backend();
+
+=head1 AUTHOR
+
+Gauthier Voron <gauthier.voron@mnesic.fr>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2016 by Gauthier Voron
+
+This library is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+=cut
