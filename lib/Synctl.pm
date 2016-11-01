06d647d46f0e364da243921a7d77565d9734a6fe
Gauthier Voron <gauthier.voron@lip6.fr>
2016-11-01 14:28:59 +0100

    doc(synctl): prepare to release a new version
    Modify the changlelog, README and version number to prepare the new
    version release.

--- lib/Synctl.pm
+++ lib/Synctl.pm
@@ -64,7 +64,7 @@ our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
 our @EXPORT = qw(
 );
 
-our $VERSION = '2.0.0';
+our $VERSION = '1.0.0';
 our $AUTHOR  = 'Gauthier Voron';
 our $MAILTO  = 'gauthier.voron@mnesic.fr';
 
